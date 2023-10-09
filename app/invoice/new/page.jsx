b9cea2567404927f3d03c40fe5128c1f6cf95780
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-10-09 08:00:19 +0300

    finished preview and form

--- app/invoice/new/page.jsx
+++ app/invoice/new/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import FormPreview from "@/components/FormPreview";
+import FormTable from "@/components/FormTable";
+import { CldImage, CldUploadButton } from "next-cloudinary";
 import { useState } from "react";
 import {
   AiOutlineCloudDownload,
@@ -8,8 +10,51 @@ import {
 } from "react-icons/ai";
 import { BsLayoutTextWindowReverse } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
-export default function Page() {
+export default function CreateInvoice() {
+  const [logoUrl, setLogoUrl] = useState("");
   const [preview, setPreview] = useState(false);
+  const [formData, setFormData] = useState({
+    companyName: "",
+    invoiceAuthor: "",
+    companyAddress: "",
+    companyCity: "",
+    companyCountry: "",
+    clientCompany: "",
+    clientAddress: "",
+    clientCity: "",
+    clientCountry: "",
+    invoiceNumber: "",
+    invoiceDate: "",
+    invoiceDueDate: "",
+  });
+  const [tableData, setTableData] = useState([]);
+  const [combinedData, setCombinedData] = useState({});
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    // console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+    // console.log(formData);
+  }
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+    const allFormData = {
+      ...formData,
+      logoUrl,
+      tableData,
+    };
+    setCombinedData(allFormData);
+    console.log(allFormData);
+    setPreview(!preview);
+  }
+  console.log(combinedData);
+
+  function updateTableData(newTableData) {
+    setTableData(newTableData);
+  }
+
+  console.log(tableData);
+
   return (
     <div className="bg-slate-50 py-8 md:py-8 px-4 md:px-16">
       {/* HEADER */}
@@ -55,30 +100,49 @@ export default function Page() {
       {/* INVOICE FORM */}
 
       {preview ? (
-        <FormPreview />
+        <FormPreview data={combinedData} />
       ) : (
-        <form className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+        <form
+          onSubmit={handleFormSubmit}
+          className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-auto"
+        >
           {/* Image & Invoice Label */}
           <div className="flex justify-between items-center">
             {/* Image */}
 
             <div className="flex items-center justify-center ">
-              <label
-                for="dropzone-file"
-                className="flex flex-col items-center justify-center w-48 h-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 "
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <AiOutlineCloudUpload className="w-6 h-6  text-gray-500 dark:text-gray-400" />
+              {logoUrl ? (
+                <CldImage
+                  width="240"
+                  height="240"
+                  src={logoUrl}
+                  alt="Invoice Logo"
+                />
+              ) : (
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-48 h-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 "
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <AiOutlineCloudUpload className="w-6 h-6  text-gray-500 dark:text-gray-400" />
 
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Upload Logo</span>
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    PNG (240x240px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <CldUploadButton
+                        onUpload={(data) => {
+                          console.log(data.info.secure_url);
+                          setLogoUrl(data.info.secure_url);
+                        }}
+                        className=""
+                        uploadPreset="InvoicePreset"
+                      />
+                    </p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                      PNG (240x240px)
+                    </p>
+                  </div>
+                  {/* <input id="dropzone-file" type="file" className="hidden" /> */}
+                </label>
+              )}
             </div>
 
             <h2 className="text-4xl uppercase font-semibold">Invoice</h2>
@@ -89,26 +153,41 @@ export default function Page() {
               className="h-7 text-base border-0 p-1 mb-2 placeholder:text-slate-400 "
               type="text"
               placeholder="Your Company"
+              name="companyName"
+              onChange={handleInputChange}
+              value={formData.companyName}
             />
             <input
               className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
               type="text"
               placeholder="Your Name"
+              name="invoiceAuthor"
+              onChange={handleInputChange}
+              value={formData.invoiceAuthor}
             />
             <input
               className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
               type="text"
               placeholder="Company Address"
+              name="companyAddress"
+              onChange={handleInputChange}
+              value={formData.companyAddress}
             />
             <input
               className="h-7 text-base border-0 p-1 mb-2 placeholder:text-slate-400"
               type="text"
               placeholder="City,State zip"
+              name="companyCity"
+              onChange={handleInputChange}
+              value={formData.companyCity}
             />
             <input
               className="h-7 text-base border-0 p-1 mb-2 placeholder:text-slate-400"
               type="text"
               placeholder="Country eg USA"
+              name="companyCountry"
+              onChange={handleInputChange}
+              value={formData.companyCountry}
             />
           </div>
           {/* CLIENT dETAILS */}
@@ -119,21 +198,33 @@ export default function Page() {
                 className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
                 type="text"
                 placeholder="Your Client's Company"
+                name="clientCompany"
+                onChange={handleInputChange}
+                value={formData.clientCompany}
               />
               <input
                 className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
                 type="text"
                 placeholder="Client's Address"
+                name="clientAddress"
+                onChange={handleInputChange}
+                value={formData.clientAddress}
               />
               <input
                 className="h-7 text-base border-0 p-1 mb-2 placeholder:text-slate-400"
                 type="text"
                 placeholder="City,State zip"
+                name="clientCity"
+                onChange={handleInputChange}
+                value={formData.clientCity}
               />
               <input
                 className="h-7 text-base border-0 p-1 mb-2 placeholder:text-slate-400"
                 type="text"
                 placeholder="Country eg USA"
+                name="clientCountry"
+                onChange={handleInputChange}
+                value={formData.clientCountry}
               />
             </div>
             <div className="flex flex-col w-1/2 mt-6">
@@ -148,6 +239,9 @@ export default function Page() {
                   className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
                   type="text"
                   placeholder="INV-202"
+                  name="invoiceNumber"
+                  onChange={handleInputChange}
+                  value={formData.invoiceNumber}
                 />
               </div>
               <div className="flex gap-2">
@@ -160,6 +254,9 @@ export default function Page() {
                 <input
                   className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
                   type="date"
+                  name="invoiceDate"
+                  onChange={handleInputChange}
+                  value={formData.invoiceDate}
                 />
               </div>
               <div className="flex gap-2">
@@ -169,10 +266,21 @@ export default function Page() {
                 <input
                   className="text-base border-0 p-1 mb-2 h-7 placeholder:text-slate-400"
                   type="date"
+                  name="invoiceDueDate"
+                  onChange={handleInputChange}
+                  value={formData.invoiceDueDate}
                 />
               </div>
             </div>
           </div>
+          {/* TABLE */}
+          <FormTable updateTableData={updateTableData} />
+          <button
+            className="bg-purple-600 py-2.5 px-6 text-white rounded"
+            type="submit"
+          >
+            Create Invoice
+          </button>
         </form>
       )}
 
--- components/FormPreview.jsx
+++ components/FormPreview.jsx
@@ -1,9 +1,108 @@
+import Image from "next/image";
 import React from "react";
+import PreviewTable from "./PreviewTable";
+import { CldImage } from "next-cloudinary";
 
-export default function FormPreview() {
+export default function FormPreview({ data }) {
+  const {
+    companyName,
+    invoiceAuthor,
+    companyAddress,
+    companyCity,
+    companyCountry,
+    clientCompany,
+    clientAddress,
+    clientCity,
+    clientCountry,
+    invoiceNumber,
+    invoiceDate,
+    invoiceDueDate,
+    logoUrl,
+    tableData,
+  } = data;
   return (
-    <div class="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
-      <h2>Preview</h2>
+    <div className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+      {/* Image & Invoice Label */}
+      <div className="flex justify-between items-center">
+        {/* Image */}
+
+        <div className="flex items-center justify-center ">
+          {logoUrl && (
+            <CldImage
+              width="120"
+              height="120"
+              src={logoUrl}
+              alt="Invoice Logo"
+            />
+          )}
+        </div>
+
+        <h2 className="text-4xl uppercase font-semibold">Invoice</h2>
+      </div>
+      {/* Company Details */}
+      <div className="flex flex-col w-1/2 mt-6">
+        <div className="flex">
+          <p className="font-bold tex-base">Company Name: &nbsp;</p>
+          <p className=" tex-base">{companyName}</p>
+        </div>
+
+        <div className="flex">
+          <p className="font-bold tex-base">Your Name : &nbsp;</p>
+          <p className=" tex-base">{invoiceAuthor}</p>
+        </div>
+        <div className="flex">
+          <p className="font-bold tex-base">Company Address : &nbsp;</p>
+          <p className=" tex-base">{companyAddress}</p>
+        </div>
+        <div className="flex">
+          <p className="font-bold tex-base">Company City : &nbsp;</p>
+          <p className=" tex-base">{companyCity}</p>
+        </div>
+        <div className="flex">
+          <p className="font-bold tex-base">Company Country : &nbsp;</p>
+          <p className=" tex-base">{companyCountry}</p>
+        </div>
+      </div>
+      {/* CLIENT dETAILS */}
+      <div className="flex justify-between gap-4 mb-8">
+        <div className="flex flex-col w-1/2 mt-6">
+          <h2 className="mb-2 font-semibold">Bill To</h2>
+
+          <div className="flex">
+            <p className="font-bold tex-base">Client Company : &nbsp;</p>
+            <p className=" tex-base">{clientCompany}</p>
+          </div>
+          <div className="flex">
+            <p className="font-bold tex-base">Client Address : &nbsp;</p>
+            <p className=" tex-base">{clientAddress}</p>
+          </div>
+          <div className="flex">
+            <p className="font-bold tex-base">Client City : &nbsp;</p>
+            <p className=" tex-base">{clientCity}</p>
+          </div>
+
+          <div className="flex">
+            <p className="font-bold tex-base">Client Country : &nbsp;</p>
+            <p className=" tex-base">{clientCountry}</p>
+          </div>
+        </div>
+        <div className="flex flex-col w-1/2 mt-6">
+          <div className="flex gap-2">
+            <p className="text-slate-500 font-bold">Invoice #</p>
+            <p>{invoiceNumber}</p>
+          </div>
+          <div className="flex gap-2">
+            <p className="text-slate-500 font-bold">Invoice Date</p>
+            <p>{invoiceDate}</p>
+          </div>
+          <div className="flex gap-2">
+            <p className="text-slate-500 font-bold">Invoice Due Date</p>
+            <p>{invoiceDueDate}</p>
+          </div>
+        </div>
+      </div>
+      {/* TABLE */}
+      <PreviewTable tableData={tableData} />
     </div>
   );
 }
